f7480e3a12fa967b9364fe278c872853e8a15976
Satyam <mitravlakshay@gmail.com>
2026-03-13 11:48:38 +0530

    addjsconcept

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const nameInputs = document.querySelectorAll('input[placeholder*="Name"]');
+  const phoneInputs = document.querySelectorAll('input[placeholder*="Mobile"], input[placeholder*="Phone"]')
+  nameInputs.forEach(input => {
+    input.addEventListener("input", (e) => {
+      let value = e.target.value;
+      value = value.replace(/[^a-zA-Z\s]/g, "");
+      e.target.value = value;
+    });
+
+  });
+
+  phoneInputs.forEach(input => {
+
+    input.addEventListener("input", (e) => {
+
+      let value = e.target.value;
+      value = value.replace(/[^0-9]/g, "");
+      if (value.length > 10) {
+        value = value.slice(0, 10);
+      }
+      e.target.value = value;
+    });
+
+  });
+
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach(form => {
+
+    form.addEventListener("submit", (e) => {
+
+      const nameField = form.querySelector('input[placeholder*="Name"]');
+      const phoneField = form.querySelector('input[placeholder*="Mobile"], input[placeholder*="Phone"]');
+
+      if (nameField) {
+        const nameValue = nameField.value.trim();
+
+        if (!/^[A-Za-z\s]+$/.test(nameValue)) {
+          alert("Name must contain only alphabets");
+          e.preventDefault();
+          return;
+        }
+      }
+
+      if (phoneField) {
+        const phoneValue = phoneField.value.trim();
+
+        if (!/^[0-9]{10}$/.test(phoneValue)) {
+          alert("Phone number must be 10 digits only");
+          e.preventDefault();
+          return;
+        }
+      }
+
+    });
+
+  });
+
+});
